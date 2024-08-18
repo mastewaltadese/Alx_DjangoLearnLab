0284b20592c76e9ec2406d7a744d80c65fa333b7
mastewaltadese <mastewaltadese7@gmail.com>
2024-08-18 12:14:30 -0700

    User Authentication in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render
 from .models import Library,  Book
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 def list_books(request):
     books = Book.objects.all()
@@ -10,3 +12,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+ 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form}) 
