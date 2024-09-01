f4e6d0880c2c302ba923b02bc7a4b74139803230
mastewaltadese <mastewaltadese7@gmail.com>
2024-09-01 12:22:59 -0700

    test

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
