9489c6d8a7e94f9070fb4ee1c6b7ee6c5339e90d
mastewaltadese <mastewaltadese7@gmail.com>
2024-09-12 12:03:26 -0700

    teast

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,5 +4,5 @@ from .models import Comment Post
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']
+        fields = ['title', 'content', 'tags', 'TagWidget()', 'widgets']
 
