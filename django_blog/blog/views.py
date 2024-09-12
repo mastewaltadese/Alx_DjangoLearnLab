605aa784a5e34b909ef0240b111af7bd4cb504d8
mastewaltadese <mastewaltadese7@gmail.com>
2024-09-12 07:15:09 -0700

    User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# blog/views.py
+from django.contrib.auth import views as auth_views
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django import forms
 
-# Create your views here.
+# Custom registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created!")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile view
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
