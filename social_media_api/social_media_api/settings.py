a5b619ded6f3412ae10a6ef748f34d8d6bbe5eb2
mastewaltadese <mastewaltadese7@gmail.com>
2024-09-22 06:45:01 -0700

    test

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -45,6 +45,11 @@ INSTALLED_APPS = [
 ]
 INSTALLED_APPS += ['rest_framework.authtoken']
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
