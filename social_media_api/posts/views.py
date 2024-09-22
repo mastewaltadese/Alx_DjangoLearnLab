7469ff3207580e73bdd5d399548b597c1cb5bf53
mastewaltadese <mastewaltadese7@gmail.com>
2024-09-22 07:53:30 -0700

    teste

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,9 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework import filters
 
+Post.objects.filter(author__in=following_users).order_by
+following.all()
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
